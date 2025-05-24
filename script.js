06db285fc4cdcff1ffeff30a53f50c479cfa091f
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2025-05-24 14:37:02 +0530

    Added Weight Category

--- script.js
+++ script.js
@@ -6,13 +6,24 @@ form.addEventListener("submit", (e) => {
   const height = parseInt(document.querySelector("#height").value);
   const weight = parseInt(document.querySelector("#weight").value);
   const results = document.querySelector("#results");
+  const category = document.querySelector("#category");
+
+  let bmi;
 
   if (height === "" || height < 0 || isNaN(height)) {
     results.innerHTML = "Please provide the Valid height";
   } else if (weight === "" || weight < 0 || isNaN(weight)) {
-    results.innerHTML = "Please provide the Valid height";
+    results.innerHTML = "Please provide the Valid weight";
   } else {
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    bmi = (weight / ((height * height) / 10000)).toFixed(2);
     results.innerHTML = `<span>${bmi}</span>`;
+  }
+
+  if (bmi < 18.6) {
+    category.innerHTML = "Under Weight";
+  } else if (bmi >= 18.6 && bmi <= 24.9) {
+    category.innerHTML = "Normal Range";
+  } else {
+    category.innerHTML = "Over Weight";
   }
 });
